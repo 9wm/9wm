c724fbeccb441fa438f590d1d6a3db62217efcf9
Jacob Adams <tookmund@gmail.com>
2016-08-03 22:29:05 -0400

    Version 1.3.6

--- 9wm.c
+++ 9wm.c
@@ -16,7 +16,7 @@
 #include "fns.h"
 
 char *version[] = {
-	"9wm version 1.3.5, Copyright © 2015 multiple authors", 0,
+	"9wm version 1.3.6, Copyright © 2016 multiple authors", 0,
 };
 
 Display *dpy;
